52caecd86c849f9eaf8f7d02a2521aa56ccc6f2f
maayankfir <mayankfir@gmail.com>
2018-06-27 10:06:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -79,21 +79,21 @@ else if (e.which === RIGHT_ARROW) {
   }
 
 function moveDodgerLeft() {
-  window.requestAnimationFrame(function() {
-    const left = positionToInteger(DODGER.style.left);
-    if (left >= 0) {
+  const left = positionToInteger(DODGER.style.left);
+    if (left > 0) {
       DODGER.style.left = '${left - 4}px';
+      window.requestAnimationFrame(moveDodgerLeft);
     }
-  });
+  }
 }
 
 function moveDodgerRight() {
-window.requestAnimationFrame(function() {
   const left = positionToInteger(DODGER.style.left);
   if ( left <= 360) {
     DODGER.style.left = '${left + 4}px';
+    window.requestAnimationFrame(moveDodgerRight);
   }
-});
+}
 }
 
 

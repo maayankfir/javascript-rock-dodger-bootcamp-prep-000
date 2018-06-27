22a71161f3bfb6b4b40345f45fdd14e85dead749
maayankfir <mayankfir@gmail.com>
2018-06-27 10:34:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,8 +23,12 @@ function checkCollision(rock) {
 
     if ((rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge)
        || (rockLeftEdge >= dodgerLeftEdge &&  rockRightEdge <= dodgerRightEdge)
-       || (rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge)) {
+       || (rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge)) 
+{
       return true;
+    }
+    else {
+      return false
     }
   }
 }
